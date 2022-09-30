40b1ae9f8dd91e5522048318a944cf0ed3987d50
Rahul Imran <notlutitious@gmail.com>
2022-09-30 21:05:51 +0100

    Feat: Add support for generating prime numbers

--- primes.py
+++ primes.py
@@ -3,4 +3,12 @@
 
 def primes(number_of_primes):
     list = []
+    i = 2
+    while len(list) < number_of_primes:
+        for j in range(2, i):
+            if i % j == 0:
+                break
+        else:
+            list.append(i)
+        i += 1
     return list
